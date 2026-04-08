5391fbd762435a87a5b60a630ec512b23abbd493
Samuel de Souza Rodrigues <67475546+samucabsb@users.noreply.github.com>
2026-04-08 10:00:57 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// =============================================================
-//  logout.php - Encerrar sessao
-// =============================================================
-session_start();
-session_destroy();
-header('Location: login.php');
-exit();
-?>
